c31106e1fef421d2e4faff1a74a734a5b1abb669
Tomasz Lubowiecki <tomasz@lubowiecki.de>
2022-11-23 15:44:14 +0100

    Updates

--- src/main/java/de/lubowiecki/einkaufsliste/App.java
+++ src/main/java/de/lubowiecki/einkaufsliste/App.java
@@ -9,22 +9,24 @@ public class App {
 
         // mit "new" baut man aus einer Klasse ein Objekt
 
-
-
         System.out.println("Shoppinglist");
 
         // Bevor ich Daten in einem Objekt ablegen kann muss es instanziert werden
-        Item i1 = new Item();
+        Item i1 = new Item(); // Konstruktor wird benutzt
 
         // Informationen in ein Objekt schreiben
-        i1.name = "Milch 1,5% Fett"; // Der Alte Wert wird überschrieben
-        i1.category = "Milchprodukte";
-        i1.amount = 10;
+        i1.setName("Milch 1,5% Fett"); // Der Alte Wert wird überschrieben
+        i1.setCategory("Milchprodukte");
+        i1.setAmount(10);
 
         Item i2 = new Item();
-        i2.name = "Honig";
-        i2.category = "Naturprodukte";
-        i2.amount = 1;
+        i2.setName("Honig");
+        i2.setCategory("Naturprodukte");
+        i2.setAmount(1);
+
+        i2.setAmount(-10);
+        i2.setCategory(null);
+        i2.setName(null);
 
         // Eigenschaft des Obejtes i1
         //i1.amount
@@ -35,14 +37,15 @@ public class App {
         int amount = 100; // Test
 
         // Informationen aus einem Objekt lesen
-        //System.out.println(i1.name + ", " + i1.category + ", Anzahl: " + i1.amount);
-        //System.out.println(i2.name + ", " + i2.category + ", Anzahl: " + i2.amount);
+        System.out.println("i1: " + i1.getName() + ", " + i1.getCategory() + ", Anzahl: " + i1.getAmount());
+        System.out.println("i2: " + i2.getName() + ", " + i2.getCategory() + ", Anzahl: " + i2.getAmount());
 
         System.out.println(i1.showDetails()); // Methoden haben Klammern für Parameter
         System.out.println(i2.showDetails()); // Methoden haben Klammern für Parameter
         // Eigenschaften haben KEINE Klammern
 
-        //System.out.println(i1);
+        System.out.println(i1); // Es wird aurtomatisch die toString-Methode verwendet
+        //System.out.println(i1.toString());
 
     }
 }
--- src/main/java/de/lubowiecki/einkaufsliste/Item.java
+++ src/main/java/de/lubowiecki/einkaufsliste/Item.java
@@ -2,14 +2,71 @@ package de.lubowiecki.einkaufsliste;
 
 public class Item {
 
-    String name;
+    // private heißt nur Objekte dieser Klasse dürfen auf diese Eigenschaften zugreifen
 
-    String category;
+    private String name;
 
-    int amount;
+    private String category;
 
-    String showDetails() {
+    private int amount;
+
+    // Wird kein Konstruktor definiert gibt es einen leeren Standardkonstruktor
+
+    // Wird ein Konstruktor definiert, wird ein leerer Standardkonstruktor nicht mehr automatisch erzeugt
+
+    // Konstruktor: wird bei der Instanzierung des Objektes benutzt
+    public Item() {
+        name = "Unbekannt";
+        category = "Unbekannt";
+    }
+
+    public Item(String name, String category) {
+        this.name = name;
+        this.category = category;
+    }
+
+    // Aufgabe: Definiere einen neuen Konstrutor mit 3 Parametern
+
+    public String showDetails() {
+        // Es gibt keine gleichnamigen Methodenparameter d.h. name ist die Instanzvariable
         return name + ", " + category + ", Anzahl: " + amount;
     }
 
+    public String getName() { // Lesend
+        return name;
+    }
+
+    public void setName(String name) { // Schreibend
+        // Prüfen ob der neue Wert ok ist und eingesetzt werden kann
+        if(name != null) {
+            // this.name ist die Instanzvariable
+            // name ist lokale Methodenparameter
+            this.name = name;
+        }
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        if(category != null) {
+            this.category = category;
+        }
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        if(amount >= 0) {
+            this.amount = amount;
+        }
+    }
+
+    @Override
+    public String toString() { // Konvertiert ein Item in einen String
+        return showDetails(); // Verwendet eine andere Methode
+    }
 }
